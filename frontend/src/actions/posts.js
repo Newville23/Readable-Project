86d1a2ae13135d5148c9ad2af0547df6d15fab26
newville23 <newville21@gmail.com>
2017-11-19 23:53:50 -0500

    feat: Add Posts Reducer

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -2,28 +2,64 @@ import * as ReadableAPI from './utils/api'
 
 export const FETCH_POST = 'FETCH_POST'
 export const FETCH_POSTS = 'FETCH_POSTS'
-export const VOTE_POST = 'VOTE_POST'
 export const CREATE_POST = 'CREATE_POST'
 export const DELETE_POST = 'DELETE_POST'
 export const EDIT_POST = 'EDIT_POST'
+export const VOTE_POST = 'VOTE_POST'
 
-export function createPost(post) {
-    return {
-        type: CREATE_POST,
-        post
+export function fetchPost(postId) {
+    const request = ReadableAPI.fetchPost(postId) 
+    return dispatch => {
+        request.then((data) => {
+            dispatch({type: FETCH_POST, payload: data})
+        })
+    }
+}
+
+export function fetchPosts() {
+    const request = ReadableAPI.fetchPosts()
+    return dispatch => {
+        request.then((data) => {
+            dispatch({type: FETCH_POSTS, payload: data})
+        })
+    }
+}
+
+export function createPost(option) {
+    option.id = postId
+    option.delete = false
+    option.timeStamp = Date.now()
+    const request = ReadableAPI.addPost(option)
+    return dispatch => {
+        request.then((data) => {
+            dispatch({type: CREATE_POST, payload: data})
+        })
     }
 }
 
 export function deletePost(postId) {
-    return {
-        type: DELETE_POST,
-        postId
+   const request = ReadableAPI.deletePost(postId)
+   return dispatch => {
+       request.then((data) => {
+           dispatch({type: DELETE_POST, payload: data})
+       })
+   }
+}
+
+export function editPost(option, postId) {
+    const request = ReadableAPI.putPost(option, postId)
+    return dispatch => {
+        request.then((data) => {
+            dispatch({type: EDIT_POST, payload: data})
+        })
     }
 }
 
-export function editPost(postId) {
-    return {
-        type: EDIT_POST,
-        postId
+export function votepost(option, postId) {
+    const rquest = ReadableAPI.votePost(option, postId)
+    return dispatch => {
+        request.then((data) => {
+            dispatch({type: VOTE_POST, payload: data})
+        })
     }
 }

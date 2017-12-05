d63383470bb807dcea24d91fd594c46578db73c8
newville23 <newville21@gmail.com>
2017-12-05 16:42:32 -0500

    fix: Remove Link import from Home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import {
-    Link,
-  } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/posts';
 import CategoryList from '../containers/CategoryList';

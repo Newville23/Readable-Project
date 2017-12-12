09fe61a0cdc60066a1578ec7df9db407caa5c2fc
newville23 <newville21@gmail.com>
2017-12-11 23:26:10 -0500

    feat: Add PostDetails icons Button

--- frontend/src/components/PostDetails.js
+++ frontend/src/components/PostDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import IconButton from 'material-ui/IconButton';
 
 const PostDetails = (props) => {
     const {allIds, byId} = props.posts;
@@ -11,6 +12,23 @@ const PostDetails = (props) => {
                         <p>{byId[post].body}</p>
                         <p> {byId[post].author}</p>
                         <p>{byId[post].voteScore} votes</p>
+                        <ul className="post-actions">
+                            <li>
+                                <IconButton iconClassName="muidocs-icon-custom-github" />
+                            </li>
+                            <li>
+                                <IconButton iconClassName="muidocs-icon-custom-github" />
+                            </li>
+                            <li>
+                                <IconButton iconClassName="muidocs-icon-custom-github" />
+                            </li>
+                            <li>
+                                <IconButton iconClassName="muidocs-icon-custom-github" />
+                            </li>
+                            <li>
+                                <IconButton iconClassName="muidocs-icon-custom-github" />
+                            </li>
+                        </ul>
                     </div>
                ))
             }

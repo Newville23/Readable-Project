07d092055c3ab7dc9aaccf1c63feaaed3132789b
newville23 <newville21@gmail.com>
2017-12-21 22:52:40 -0500

    feat: Add create mechanism to the App

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -1,27 +1,88 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
 import TextField from 'material-ui/TextField';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const categories = [
+    {
+      name: 'react',
+      path: 'react'
+    },
+    {
+      name: 'redux',
+      path: 'redux'
+    },
+    {
+      name: 'udacity',
+      path: 'udacity'
+    }
+]; 
 
 class PostForm extends Component {
+
+    state = {
+            title: '',
+            author: '',
+            category: 'react',
+            body: '',
+    };
+
+    handleOnChange = name => event => {
+        console.log(name)
+        const target = event.target;
+        if(name === 'category') {
+            var value = target.innerHTML;
+        }else{
+            var value = target.value;
+        }
+        this.setState({
+            [name]: value,
+        });
+    }
+    handleSubmit = () => {
+         const {createPost} = this.props;
+         createPost(this.state);
+    }
     render () {
         return(
             <div>
+                <form onSubmit={() => this.handleSubmit()}>
                     <TextField
-                    hintText="Password Field"
-                    floatingLabelText="Password"
-                    type="password"
-                /><br />
-                <TextField
-                    hintText="MultiLine with rows: 2 and rowsMax: 4"
-                    multiLine={true}
-                    rows={2}
-                    rowsMax={4}
-                /><br />
-                <TextField
-                    hintText="Message Field"
-                    floatingLabelText="MultiLine and FloatingLabel"
-                    multiLine={true}
-                    rows={2}
-                />
+                        floatingLabelText="Title"
+                        onChange={this.handleOnChange('title')}
+                        value={this.state.title}
+                        name="title"
+                    />
+                    <br />
+                    <TextField
+                        floatingLabelText="Author"
+                        onChange={this.handleOnChange('author')}
+                        value={this.state.author}
+                        name="author"
+                    />
+                    <br />
+                    <DropDownMenu value={this.state.category} onChange={this.handleOnChange('category')}>
+                        {categories.map(option => (
+                            <MenuItem key={option.name} value={option.path} primaryText={option.path}/>
+                        ))}
+                    </DropDownMenu>
+                  <br/>
+                   <TextField
+                        floatingLabelText="Body"
+                        multiLine={true}
+                        rows={2}
+                        onChange={this.handleOnChange('body')}
+                        value={this.state.body}
+                        name="body"
+                    />
+
+                    <Link to="/" onClick={() => this.handleSubmit()}>
+                        <RaisedButton label="Add" primary={true} />
+                    </Link>    
+                </form>
+                
             </div>
         )
     }
